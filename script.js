8a6e8c47098e7acc0a1cfb10a6eeb50353a83dc3
alessiaimprota <alessiaimprota126@gmail.com>
2025-12-04 12:36:29 +0100

    2nd ex

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
-
+/*
 const numeroUno = parseInt(prompt("Inserisci il primo numero"));
 const numeroDue = parseInt(prompt("inserisci il secondo numero"));
 console.log(typeof numeroDue, typeof numeroUno);
@@ -13,3 +13,14 @@ console.log(`i numeri sono uguali`)
 else {
     console.log(`il numero maggiore è ${numeroDue}`)
 };
+*/
+/* Il software deve chiedere per 5 volte all’utente di inserire un numero. 
+ Il programma stampa la somma di tutti i numeri inseriti. 
+ Esegui questo programma in due versioni, con il for e con il while (facoltativo)*/
+
+ let sommaNumeri=0;
+ for(let i=0; i < 5; i++){
+    let numero = parseInt (prompt("Inserisci un numero"));
+    sommaNumeri += numero;
+}
+console.log(`${sommaNumeri}`)
